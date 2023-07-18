ed1967863b5971e7d4b4ffe40d9b1de01e5f4e07
Muhammad Kamran <mkrnaqeebi@gmail.com>
2023-07-18 06:35:44 +0500

    fix to work with python3+

--- celebrity_images_download.py
+++ celebrity_images_download.py
@@ -1,12 +1,12 @@
 # Author: Prateek Mehta
+# Edited by: Muhammad Kamran
 # Script to download images from google
 
 
 import time       #Importing the time library to check the time of code execution
-import sys    #Importing the System Library
 import os
-import urllib2
-
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
 
 ########### Edit From Here ###########
 
@@ -23,59 +23,19 @@ keywords = [' face', ' side face', ' looking up', ' looking down', ' wearning gl
 
 #Downloading entire Web Document (Raw Page Content)
 def download_page(url):
-    version = (3,0)
-    cur_version = sys.version_info
-    if cur_version >= version:     #If the Current Version of Python is 3.0 or above
-        import urllib.request    #urllib library for Extracting web pages
-        try:
-            headers = {}
-            headers['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
-            req = urllib.request.Request(url, headers = headers)
-            resp = urllib.request.urlopen(req)
-            respData = str(resp.read())
-            return respData
-        except Exception as e:
-            print(str(e))
-    else:                        #If the Current Version of Python is 2.x
-        import urllib2
-        try:
-            headers = {}
-            headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-            req = urllib2.Request(url, headers = headers)
-            response = urllib2.urlopen(req)
-            page = response.read()
-            return page
-        except:
-            return"Page Not found"
-
-
-#Finding 'Next Image' from the given raw page
-def _images_get_next_item(s):
-    start_line = s.find('rg_di')
-    if start_line == -1:    #If no links are found then give an error!
-        end_quote = 0
-        link = "no_links"
-        return link, end_quote
-    else:
-        start_line = s.find('"class="rg_meta"')
-        start_content = s.find('"ou"',start_line+1)
-        end_content = s.find(',"ow"',start_content+1)
-        content_raw = str(s[start_content+6:end_content-1])
-        return content_raw, end_content
-
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+    req = Request(url, headers = headers)
+    resp = urlopen(req)
+    respData = str(resp.read())
+    return respData
 
 #Getting all links with the help of '_images_get_next_image'
 def _images_get_all_items(page):
-    items = []
-    while True:
-        item, end_content = _images_get_next_item(page)
-        if item == "no_links":
-            break
-        else:
-            items.append(item)      #Append all the links in the list named 'Links'
-            time.sleep(0.1)        #Timer could be used to slow down the request for image downloads
-            page = page[end_content:]
-    return items
+    # from html string get all image links which links contains 'encrypted-tbn0.gstatic.com/images'
+    soup = BeautifulSoup(page, 'html.parser')
+    image_links = [img['src'] for img in soup.find_all('img') if 'encrypted-tbn0.gstatic.com/images' in img['src']]
+    return image_links
 
 
 ############## Main Program ############
@@ -104,9 +64,9 @@ while i<len(search_keyword):
     while j<len(keywords):
         pure_keyword = keywords[j].replace(' ','%20')
         url = 'https://www.google.com/search?q=' + search + pure_keyword + '&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'
-        raw_html =  (download_page(url))
+        raw_html = download_page(url)
         time.sleep(0.1)
-        items = items + (_images_get_all_items(raw_html))
+        items = items + _images_get_all_items(raw_html)
         j = j + 1
     #print ("Image Links = "+str(items))
     print ("Total Image Links = "+str(len(items)))
@@ -123,55 +83,21 @@ while i<len(search_keyword):
     print("Total time taken: "+str(total_time)+" Seconds")
     print ("Starting Download...")
 
-    ## To save imges to the same directory
-    # IN this saving process we are just skipping the URL if there is any error
-
     k=0
     errorCount=0
     while(k<len(items)):
-        from urllib2 import Request,urlopen
-        from urllib2 import URLError, HTTPError
+        req = Request(items[k], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
+        response = urlopen(req,None,15)
+        output_file = open(search_keywords+"/"+str(k+1)+".jpg",'wb')
+        
+        data = response.read()
+        output_file.write(data)
+        response.close()
 
-        try:
-            req = Request(items[k], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
-            response = urlopen(req,None,15)
-            output_file = open(search_keywords+"/"+str(k+1)+".jpg",'wb')
-            
-            data = response.read()
-            output_file.write(data)
-            response.close();
+        print("completed ====> "+str(k+1))
 
-            print("completed ====> "+str(k+1))
-
-            k=k+1;
-
-        except IOError:   #If there is any IOError
-
-            errorCount+=1
-            print("IOError on image "+str(k+1))
-            k=k+1;
-
-        except HTTPError as e:  #If there is any HTTPError
-
-            errorCount+=1
-            print("HTTPError"+str(k))
-            k=k+1;
-        except URLError as e:
-
-            errorCount+=1
-            print("URLError "+str(k))
-            k=k+1;
-
+        k=k+1
     i = i+1
 
 print("\n")
 print("Everything downloaded!")
-print("\n"+str(errorCount)+" ----> total Errors")
-
-#----End of the main program ----#
-
-
-# In[ ]:
-
-
-
